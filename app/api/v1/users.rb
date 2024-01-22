5c6f6a0092492ed9fcd00746bc03a2b7b47a5b50
Piyush Kumar <piyush.kumar@gocomet.com>
2024-01-22 17:04:38 +0530

    following and followers working, feature for user follow set.

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -5,14 +5,51 @@ class USERS<Grape::API
           User.all
       end
 
-      desc "this is to get all details of a specific user"
-      post "follow"do
-
-      end
-
       desc"Edit a user within itself"
       put do
 
+      end
+
+
+
+
+
+      # get all of the user followed
+      params  do
+            requires :id , type:Integer
+      end
+      post "followed" do
+            V1::Services::UserFollow.get_all_followed(params[:id])
+      end
+
+
+
+      # get all of the user followers
+      params  do
+            requires :id , type:Integer
+      end
+      post "followers" do
+            V1::Services::UserFollow.get_all_followers(params[:id])
+      end
+
+
+
+      params do
+            requires :follower_id , type: Integer
+            requires :followed_id , type: Integer
+      end
+      post "follow" do
+
+            V1::Services::UserFollow.follow_user(params[:follower_id],params[:followed_id]);
+      end
+
+
+      params do
+            requires :follower_id , type: Integer
+            requires :followed_id , type: Integer
+      end
+      post "unfollow" do
+            V1::Services::UserFollow.unfollow_user(params[:follower_id],params[:followed_id]);
       end
     end
 
